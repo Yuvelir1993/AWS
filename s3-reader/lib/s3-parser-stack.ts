df4b74cf3bfa5adfc190dd454ffcd6bf6780a15a
plozovik <Pavlo.Lozovikov@boc-pl.com>
2024-09-17 18:17:34 +0200

    modularized stack

--- s3-reader/lib/s3-parser-stack.ts
+++ s3-reader/lib/s3-parser-stack.ts
@@ -1,42 +1,103 @@
-import * as cdk from "aws-cdk-lib";
+import * as cdk from 'aws-cdk-lib';
 import ec2 = require("aws-cdk-lib/aws-ec2");
 import ecs = require("aws-cdk-lib/aws-ecs");
-import { Construct } from "constructs";
+import * as elbv2 from "aws-cdk-lib/aws-elasticloadbalancingv2";
+import { Construct } from 'constructs';
+import path = require('path');
+
+const config = {
+  apiPort: 5001,
+  hostPort: 5001,
+  loadBalancerPort: 80,
+  webAppBackendName: "WebAppBackend"
+};
 
 export class S3ParserStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const vpc = new ec2.Vpc(this, "S3-reader-app-VPC");
+    const vpc = this.createVpc();
+    const ecsCluster = this.createEcsCluster(vpc);
+    const taskDefinitionBackendAPI = this.createBackendTaskDefinition();
+    const ecsFargateBackendAPIService = this.createFargateService(ecsCluster, taskDefinitionBackendAPI);
 
-    const ecsCluster = new ecs.Cluster(this, "MyS3apiECSCluster", {
-      clusterName: "S3-API-Cluster",
-      vpc: vpc,
-      capacity: {
-        instanceType: ec2.InstanceType.of(
-          ec2.InstanceClass.T3,
-          ec2.InstanceSize.MICRO
-        ),
-      },
+    this.createApplicationLoadBalancer(vpc, ecsFargateBackendAPIService);
+  }
+
+  private createVpc (): ec2.Vpc {
+    return new ec2.Vpc(this, "WebAppVPC", { maxAzs: 2 });
+  }
+
+  private createEcsCluster (vpc: ec2.Vpc): ecs.Cluster {
+    return new ecs.Cluster(this, "WebAppECSCluster", {
+      clusterName: "Web-App-Cluster",
+      vpc: vpc
+    });
+  }
+
+  private createBackendTaskDefinition (): ecs.FargateTaskDefinition {
+    const taskDefinition = new ecs.FargateTaskDefinition(this, `${config.webAppBackendName}TaskDef`, {
+      family: `${config.webAppBackendName}Family`,
+      cpu: 256,
+      memoryLimitMiB: 512
     });
 
-    const taskDefinition = new ecs.Ec2TaskDefinition(this, "TaskDef", {
-      family: "S3ReaderBackend",
-    });
-
-    taskDefinition.addContainer("DefaultContainer", {
-      image: ecs.ContainerImage.fromAsset("../assets/s3-api/Dockerfile", {
+    taskDefinition.addContainer(`${config.webAppBackendName}APIContainer`, {
+      image: ecs.ContainerImage.fromAsset(path.resolve(__dirname, '../assets/s3-api'), {
         buildArgs: {
-          API_PORT: "5000",
-          // "S3_BUCKET": "XXXXXXXXXXXXXXXX"
+          API_PORT: `${config.apiPort}`
         },
       }),
-      memoryLimitMiB: 512,
+      portMappings: [{ containerPort: config.apiPort, hostPort: config.hostPort }]
     });
 
-    const ecsService = new ecs.Ec2Service(this, "S3ReaderBackendService", {
-      cluster: ecsCluster,
-      taskDefinition,
+    return taskDefinition;
+  }
+
+  private createFargateService (cluster: ecs.Cluster, taskDefinition: ecs.FargateTaskDefinition): ecs.FargateService {
+    const service = new ecs.FargateService(this, `${config.webAppBackendName}ApiService`, {
+      cluster: cluster,
+      taskDefinition: taskDefinition,
+    });
+
+    service.autoScaleTaskCount({ maxCapacity: 2 });
+    return service;
+  }
+
+  private createApplicationLoadBalancer (vpc: ec2.Vpc, ecsService: ecs.FargateService): void {
+    const loadBalancer = new elbv2.ApplicationLoadBalancer(
+      this,
+      `${config.webAppBackendName}APIService-internal-elbv2`,
+      {
+        vpc,
+        internetFacing: true,
+      }
+    );
+
+    const listener = loadBalancer.addListener(
+      `${config.webAppBackendName}APIService-http-api-listener`,
+      {
+        port: config.loadBalancerPort,
+        open: true,
+        defaultAction: elbv2.ListenerAction.fixedResponse(404),
+      }
+    );
+
+    listener.addTargets(
+      `${config.webAppBackendName}APIService-target-group`,
+      {
+        port: config.loadBalancerPort,
+        healthCheck: {
+          path: "/health",
+          interval: cdk.Duration.seconds(30),
+          timeout: cdk.Duration.seconds(3),
+        },
+        targets: [ecsService]
+      }
+    );
+
+    new cdk.CfnOutput(this, "LoadBalancerDNS", {
+      value: loadBalancer.loadBalancerDnsName,
     });
   }
 }
