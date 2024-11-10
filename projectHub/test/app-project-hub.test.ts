a35960c445bbea16a10680bbddc9af9b7c6b0a32
plozovik <plozovikov@gmail.com>
2024-11-10 17:41:26 +0100

    renamed 's3-parser' to 'app-project-hub'

--- projectHub/test/app-project-hub.test.ts
+++ projectHub/test/app-project-hub.test.ts
@@ -1,9 +1,9 @@
 // import * as cdk from 'aws-cdk-lib';
 // import { Template } from 'aws-cdk-lib/assertions';
-// import * as S3Parser from '../lib/s3-parser-stack';
+// import * as S3Parser from '../lib/app-project-hub-stack';
 
 // example test. To run these tests, uncomment this file along with the
-// example resource in lib/s3-parser-stack.ts
+// example resource in lib/app-project-hub-stack.ts
 test('SQS Queue Created', () => {
 //   const app = new cdk.App();
 //     // WHEN
