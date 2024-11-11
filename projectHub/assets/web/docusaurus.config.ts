41616762a1b6c9dedf5509213de152af613caf91
plozovik <plozovikov@gmail.com>
2024-11-11 16:42:46 +0100

    fixed style applied from tailwind + minor changes liek renamings or so

--- projectHub/assets/web/docusaurus.config.ts
+++ projectHub/assets/web/docusaurus.config.ts
@@ -17,8 +17,8 @@ const config: Config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: "facebook", // Usually your GitHub org/user name.
-  projectName: "docusaurus", // Usually your repo name.
+  organizationName: "plozovik", // Usually your GitHub org/user name.
+  projectName: "project-hub", // Usually your repo name.
 
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
--- projectHub/assets/web/src/components/projects/ProjectReadme.tsx
+++ projectHub/assets/web/src/components/projects/ProjectReadme.tsx
@@ -1,4 +1,3 @@
-// src/components/ProjectReadme.tsx
 import React from "react";
 import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
