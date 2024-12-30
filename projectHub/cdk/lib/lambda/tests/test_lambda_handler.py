fb2f6b27bbd4d47d3b36d9d4eef64860c2a1efc6
plozovik <plozovikov@gmail.com>
2024-12-30 20:54:33 +0100

    Lambda: checking prerequisites; added resources test doc archives

--- projectHub/cdk/lib/lambda/tests/test_lambda_handler.py
+++ projectHub/cdk/lib/lambda/tests/test_lambda_handler.py
@@ -10,6 +10,12 @@ import pytest
 # 3. if validation not passed for the not correct zip
 from moto import mock_aws
 import boto3
+from pathlib import Path
+
+CURRENT_PATH = Path(os.path.dirname(os.path.realpath(__file__)))
+
+RESOURCES_DOCS_ZIP_SAMPLE_JAVA = CURRENT_PATH / "resources" / "sampleJava-1.0.0.zip"
+RESOURCES_DOCS_ZIP_SAMPLE_PYTHON = CURRENT_PATH / "resources" / "samplePython-0.1.0.zip"
 
 
 @pytest.fixture(scope="function")
@@ -35,14 +41,31 @@ def create_bucket(s3):
 
 @pytest.mark.usefixtures("create_bucket")
 class TestUnzipValidateUpload:
-    def test_mocked_bucket_created(self, create_bucket):
-        result = boto3.client("s3").list_buckets()
-        assert len(result["Buckets"]) == 1
-    def test_unpacked_zip_has_desired_amount_of_files(self, create_bucket):
+    def test_check_prerequisites(self, create_bucket):
+        """
+        Checking necessary pre-requisites before tests.
+        """
+        buckets_amount = boto3.client("s3").list_buckets()
+        assert len(buckets_amount["Buckets"]) == 1, "There should be only 1 mock bucket created."
+        assert Path(RESOURCES_DOCS_ZIP_SAMPLE_PYTHON).is_file(), f"Sample python docu should exist! {RESOURCES_DOCS_ZIP_SAMPLE_PYTHON}"
+        assert Path(RESOURCES_DOCS_ZIP_SAMPLE_JAVA).is_file(), f"Sample java docu should exist! {RESOURCES_DOCS_ZIP_SAMPLE_JAVA}"
+
+
+    def test_unpacked_python_doc_zip_has_desired_amount_of_files(self, create_bucket, tmp_path):
+        d = tmp_path / "docs_python_unpacked"
+        d.mkdir()
         pass
+
+    def test_unpacked_java_doc_zip_has_desired_amount_of_files(self, create_bucket, tmp_path):
+        d = tmp_path / "docs_java_unpacked"
+        d.mkdir()
+        pass
+
     def test_s3_upload_file_count(self, create_bucket):
         pass
+
     def test_validation_passed_for_correct_zip(self, create_bucket):
         pass
+
     def test_validation_not_passed_for_incorrect_zip(self, create_bucket):
         pass
