98853a8cb7f9542f5df3d1028ade9260970ac75d
plozovik <plozovikov@gmail.com>
2025-01-03 20:39:52 +0100

    Lambda: test_validation_passed_for_correct_zip

--- projectHub/cdk/lib/lambda/tests/test_lambda_handler.py
+++ projectHub/cdk/lib/lambda/tests/test_lambda_handler.py
@@ -16,6 +16,7 @@ from lambda_handler import unzip_validate_upload, LambdaS3Class
 CURRENT_PATH: Path = Path(os.path.dirname(os.path.realpath(__file__)))
 RESOURCES_DOCS_ZIP_SAMPLE_JAVA: Path = Path(CURRENT_PATH / "resources" / "sampleJava-1.0.0.zip")
 RESOURCES_DOCS_ZIP_SAMPLE_PYTHON: Path = Path(CURRENT_PATH / "resources" / "samplePython-0.1.0.zip")
+RESOURCES_DOCS_ZIP_SAMPLE_NOT_VALID: Path = Path(CURRENT_PATH / "resources" / "noDocsFolder-1.0.0.zip")
 
 @pytest.fixture
 def env():
@@ -48,7 +49,7 @@ def create_bucket(s3):
     s3.resource.create_bucket(Bucket="project-hub-tests")
 
 @pytest.fixture
-def copy_resources_docs(tmp_path):
+def copy_valid_resources_docs(tmp_path):
     """
     Copies resource zip files to temporary directories within tmp_path.
 
@@ -76,10 +77,37 @@ def copy_resources_docs(tmp_path):
 
     return copied_paths
 
+@pytest.fixture
+def copy_not_valid_resources_docs(tmp_path):
+    """
+    Copies NOT VALID resource zip files to temporary directories within tmp_path.
+
+    Args:
+        tmp_path (Path): Built-in pytest fixture providing a temporary directory.
+
+    Returns:
+        dict: A dictionary mapping resource directory names to the paths of the copied files.
+    """
+    resources_docs = {
+        "nv": RESOURCES_DOCS_ZIP_SAMPLE_NOT_VALID,
+    }
+
+    copied_paths = {}
+
+    for resource_tmp_dir_name, resource_full_path in resources_docs.items():
+        dst = tmp_path / "docs" / resource_tmp_dir_name
+        dst.mkdir(parents=True, exist_ok=True)
+        shutil.copy(src=resource_full_path, dst=dst)
+        copied_file = dst / resource_full_path.name
+
+        assert copied_file.exists(), f"File {copied_file} was not copied successfully."
+        copied_paths[resource_tmp_dir_name] = copied_file
+
+    return copied_paths
 
 @pytest.mark.usefixtures("create_bucket")
 class TestUnzipValidateUpload:
-    def test_check_prerequisites(self, copy_resources_docs):
+    def test_check_prerequisites(self, copy_valid_resources_docs):
         """
         Checking necessary pre-requisites before tests:
         1. AWS mocks set up is correct.
@@ -90,15 +118,15 @@ class TestUnzipValidateUpload:
         assert Path(RESOURCES_DOCS_ZIP_SAMPLE_PYTHON).is_file(), f"Sample python docu should exist! {RESOURCES_DOCS_ZIP_SAMPLE_PYTHON}"
         assert Path(RESOURCES_DOCS_ZIP_SAMPLE_JAVA).is_file(), f"Sample java docu should exist! {RESOURCES_DOCS_ZIP_SAMPLE_JAVA}"
 
-        for resource_dir, copied_file_path in copy_resources_docs.items():
+        for resource_dir, copied_file_path in copy_valid_resources_docs.items():
             assert copied_file_path.exists(), f"Copied file for {resource_dir} does not exist."
             assert copied_file_path.is_file(), f"Copied path for {resource_dir} is not a file."
 
-    def test_unpacked_doc_zip_has_desired_amount_of_files(self, s3, copy_resources_docs, tmp_path):
+    def test_unpacked_doc_zip_has_desired_amount_of_files(self, s3, copy_valid_resources_docs, tmp_path):
         """
         Test unpacking document zips and comparing the file count with the extracted contents.
         """
-        for project_key, zip_path in copy_resources_docs.items():
+        for project_key, zip_path in copy_valid_resources_docs.items():
             base_extract_path = tmp_path / "extracted_docs" / project_key
             base_extract_path.mkdir(parents=True, exist_ok=True)
             with zipfile.ZipFile(zip_path) as archive:
@@ -119,11 +147,11 @@ class TestUnzipValidateUpload:
                 f"but {extracted_file_count} files were extracted."
             )
 
-    def test_s3_upload_file_count(self, s3, copy_resources_docs, tmp_path):
+    def test_s3_upload_file_count(self, s3, copy_valid_resources_docs, tmp_path):
         """
         Test if all objects have been uploaded to S3 for each project.
         """
-        for project_key, zip_path in copy_resources_docs.items():
+        for project_key, zip_path in copy_valid_resources_docs.items():
             base_extract_path = tmp_path / "extracted_docs" / project_key
             base_extract_path.mkdir(parents=True, exist_ok=True)
 
@@ -161,8 +189,26 @@ class TestUnzipValidateUpload:
                 f"Uploaded files = {len(uploaded_keys)}"
             )
 
-    def test_validation_passed_for_correct_zip(self):
-        pass
+    def test_validation_passed_for_correct_zip(self, s3, copy_not_valid_resources_docs, tmp_path):
+        """
+        Test if validation functionality worked correctly for not valid documentations uploaded.
+        """
+        for project_key, zip_path in copy_not_valid_resources_docs.items():
+            base_extract_path = tmp_path / "extracted_docs" / project_key
+            base_extract_path.mkdir(parents=True, exist_ok=True)
 
-    def test_validation_not_passed_for_incorrect_zip(self):
-        pass
+            unzip_validate_upload(
+                s3_resource=s3,
+                s3_key_project=f"projects/{project_key}",
+                zip_from_s3_path=zip_path,
+                path_dir_to_extract_archive_into=base_extract_path
+            )
+
+            uploaded_keys = [
+                obj.key.replace("\\", "/").replace(f"projects/{project_key}/", "")
+                for obj in s3.bucket.objects.all()
+                if obj.key.startswith(f"projects/{project_key}/")
+            ]
+
+            assert len(uploaded_keys) == 1, "On failed validation only 1 file should be uploaded!"
+            assert uploaded_keys[0] == "docs/index.html", "On failed validation only 'docs/index.html' should be uploaded!"
