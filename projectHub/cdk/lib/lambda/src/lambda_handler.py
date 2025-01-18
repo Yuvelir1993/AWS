fecbf4036910ef325c4cda0672154e4e8c435de3
plozovik <plozovikov@gmail.com>
2025-01-18 11:56:53 +0100

    fixed lambda getting object from s3

--- projectHub/cdk/lib/lambda/src/lambda_handler.py
+++ projectHub/cdk/lib/lambda/src/lambda_handler.py
@@ -7,7 +7,6 @@ import tempfile
 import urllib.parse
 import mimetypes
 from pathlib import Path
-import boto3
 from boto3 import resource
 import shutil
 
@@ -18,15 +17,15 @@ from helpers import Validator, generate_error_index_html
 
 print('Loading function...')
 
-s3 = boto3.client('s3')
+_LAMBDA_S3_RESOURCE = {"resource": resource('s3'),
+                       "bucket_name": environ.get("BUCKET_NAME", "NONE")}
 
-_LAMBDA_S3_RESOURCE = { "resource" : resource('s3'),
-                        "bucket_name" : environ.get("BUCKET_NAME","NONE") }
 
 class LambdaS3Class:
     """
     AWS S3 Resource Class
     """
+
     def __init__(self, lambda_s3_resource):
         """
         Initialize an S3 Resource
@@ -34,6 +33,7 @@ class LambdaS3Class:
         self.resource = lambda_s3_resource["resource"]
         self.bucket_name = lambda_s3_resource["bucket_name"]
         self.bucket = self.resource.Bucket(self.bucket_name)
+
 
 @dataclass
 class Project:
@@ -69,7 +69,8 @@ def handler(event: S3ObjectLambdaEvent, context: LambdaContext):
     projects_space = os.environ.get('PROJECTS_SPACE')
 
     print(f"Environment - BUCKET_NAME: {bucket_name}")
-    print(f"Environment - BUCKET_NAME (s3_resource_class): {s3_resource_class.bucket_name}")
+    print(
+        f"Environment - BUCKET_NAME (s3_resource_class): {s3_resource_class.bucket_name}")
     print(f"Environment - PROJECTS_SPACE: {projects_space}")
     print(f"Environment - DOC_LINKS_JSON: {doc_links_json}")
 
@@ -96,28 +97,35 @@ def handler(event: S3ObjectLambdaEvent, context: LambdaContext):
     print(f"Extracted Project: {project}")
 
     try:
-        generate_doc_links(doc_links_json, uploaded_object_key, project, s3_resource_class)
-        path_fetched_object_from_s3 = fetch_uploaded_object(uploaded_object_key, s3_resource_class)
+        generate_doc_links(doc_links_json, uploaded_object_key,
+                           project, s3_resource_class)
+        path_fetched_object_from_s3 = fetch_uploaded_object(
+            uploaded_object_key, s3_resource_class)
         with tempfile.TemporaryDirectory() as tmp_dir:
             path_dir_to_extract_archive_into = Path(tmp_dir)
             s3_key_project = f"{projects_space}/{project.full_name}"
-            unzip_validate_upload(s3_resource_class, s3_key_project, path_fetched_object_from_s3, path_dir_to_extract_archive_into)
+            unzip_validate_upload(s3_resource_class, s3_key_project,
+                                  path_fetched_object_from_s3, path_dir_to_extract_archive_into)
     except KeyError as index_error:
         print(f"Error 404 processing {uploaded_object_key} from bucket {
-        s3_resource_class.bucket_name}. Exception: {index_error}")
+            s3_resource_class.bucket_name}. Exception: {index_error}")
     except Exception as e:
         print(f"Error processing {uploaded_object_key} from bucket {
               s3_resource_class.bucket_name}. Exception: {e}")
         raise e
+
 
 def fetch_uploaded_object(uploaded_object_key: str, s3_resource: LambdaS3Class) -> Path:
     """
     Fetching uploaded to S3 object to a temporary directory.
     """
     try:
-        local_zip_path = Path(tempfile.gettempdir()) / Path(uploaded_object_key).name
-        s3_resource.bucket.download_file(uploaded_object_key, str(local_zip_path))
-        print(f"Downloaded '{uploaded_object_key}' from S3 to a local temp '{local_zip_path}'")
+        local_zip_path = Path(tempfile.gettempdir()) / \
+            Path(uploaded_object_key).name
+        s3_resource.bucket.download_file(
+            uploaded_object_key, str(local_zip_path))
+        print(f"Downloaded '{uploaded_object_key}' from S3 to a local temp '{
+              local_zip_path}'")
         return local_zip_path
     except Exception as e:
         print(f"An error during fetching object from s3 occurred: {e}")
@@ -130,19 +138,23 @@ def unzip_validate_upload(s3_resource: LambdaS3Class, s3_key_project: str, zip_f
     If validation fails, uploads an error index.html to the 'docs' folder.
     """
     try:
-        shutil.unpack_archive(zip_from_s3_path, path_dir_to_extract_archive_into)
+        shutil.unpack_archive(
+            zip_from_s3_path, path_dir_to_extract_archive_into)
         print(f"Extracted uploaded zip '{
               zip_from_s3_path}' to '{path_dir_to_extract_archive_into}'")
 
         validator = Validator(path_dir_to_extract_archive_into)
         if validator.validate():
-            print("Validation passed - uploaded project documentation is OK. Proceeding to upload files.")
-            print(f"The uploaded project's documentation will be uploaded into '{s3_key_project}'")
+            print(
+                "Validation passed - uploaded project documentation is OK. Proceeding to upload files.")
+            print(f"The uploaded project's documentation will be uploaded into '{
+                  s3_key_project}'")
             for root, folders, files in path_dir_to_extract_archive_into.walk():
                 for name in files:
                     file_path = Path(root / name)
                     print(f"Proceeding the file {name} located in {file_path}")
-                    file_s3_key = f"{s3_key_project}/{str(file_path.relative_to(path_dir_to_extract_archive_into))}"
+                    file_s3_key = f"{
+                        s3_key_project}/{str(file_path.relative_to(path_dir_to_extract_archive_into))}"
 
                     content_type, _ = mimetypes.guess_type(str(file_path))
                     if content_type is None:
@@ -157,12 +169,14 @@ def unzip_validate_upload(s3_resource: LambdaS3Class, s3_key_project: str, zip_f
                     )
 
                 for folder in folders:
-                    print(f"There is a folder {folder} existing after zip extraction")
+                    print(f"There is a folder {
+                          folder} existing after zip extraction")
         else:
             error_messages = validator.get_error_messages()
             error_html_content = generate_error_index_html(error_messages)
             error_index_html_s3_key = f"{s3_key_project}/docs/index.html"
-            print(f"Validation failed. Uploading error index.html with report to {error_index_html_s3_key}")
+            print(f"Validation failed. Uploading error index.html with report to {
+                  error_index_html_s3_key}")
 
             # Check if 'project.full_name' key exists in s3?
             # Potential place for step functions integration? or sending notification in any other way?
@@ -197,12 +211,13 @@ def generate_doc_links(doc_links_json, uploaded_object_key, project: Project, s3
     doc_links = []
 
     try:
-        metadata_response = s3_resource.bucket.get_object(Key=doc_links_json)
+        metadata_response = s3_resource.Object(
+            bucket_name=s3_resource.bucket_name, key=doc_links_json)
         metadata_content = metadata_response['Body'].read().decode('utf-8')
         doc_links = json.loads(metadata_content)
         print(f"Existing {doc_links_json} loaded. Version: {
             metadata_response.get('VersionId', 'N/A')}")
-    except s3.exceptions.NoSuchKey:
+    except Exception:
         print(f"{doc_links_json} does not exist.")
 
     updated = False
